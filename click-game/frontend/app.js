23b7ca351ba6db6ab57bea46a5a0032de706bbd8
stefancreteanu <stefancreteanu@yahoo.com>
2020-08-06 02:37:16 +0300

    a

--- click-game/frontend/app.js
+++ click-game/frontend/app.js
@@ -1,34 +1,17 @@
 const click = document.getElementById('counter');
 const btn = document.querySelector('.clickBtn');
 const money = document.getElementById('money');
+const auto = document.querySelector('.auto-click');
+const faster = document.querySelector('.click-faster');
+
+let autoClickBool = false;
+let cond = localStorage.getItem('condition');
 let clickCounter = 0;
 
-const auto = document.querySelector('.auto-click');
-let autoClickBool = false;
-
-function autoClick() {
-    auto.addEventListener('click', () => {
-        setInterval(function() {
-            btn.click();
-        }, 1000);
-        localStorage.counter = localStorage.counter - 3;
-        autoClickBool = true;
-
-        let cond = localStorage.getItem("condition");
-        if(autoClickBool === true) {
-            cond = true;
-        } else {
-            cond = false;
-        }
-  
-        console.log(cond);
-        localStorage.setItem("condition", cond);
-    })   
-}
 
 function increment() {
     btn.addEventListener('click', () => {
-        let n = localStorage.getItem("counter");
+        let n = localStorage.getItem('counter');
         clickCounter = n;
         if(n === null) {
             n = 1;
@@ -48,5 +31,58 @@ function increment() {
     }
 }
 
+let interval = 1000;
+
+function clickFaster() {
+    faster.addEventListener('click', () => {
+        if(localStorage.counter >= 3) {
+            interval = localStorage.getItem('interval');
+            interval = interval - 100;
+            localStorage.setItem('interval', interval);
+        }
+    })
+}
+
+function autoClick() {
+    auto.addEventListener('click', () => {
+        if(localStorage.counter >= 3) {
+            setInterval(() => {
+                let n = localStorage.getItem('counter');
+                clickCounter = n;
+                if(n === null) {
+                    n = 1;
+                    click.innerHTML = n;
+                } else {
+                    n++;
+                    click.innerHTML = n;
+                }
+                console.log(n);
+                localStorage.setItem("counter", n);
+            }, `${localStorage.interval}`);
+            console.log(interval);
+            localStorage.counter = localStorage.counter - 3;
+            click.innerHTML = localStorage.counter;
+            autoClickBool = true;
+            
+            if(autoClickBool === true) {
+                cond = true;
+            } else {
+                cond = false;
+            }
+        } else {
+            console.log("You need more than 3 clicks to purchase this upgrade");
+        }
+  
+        console.log(cond);
+        localStorage.setItem("condition", cond);
+    })
+    window.onload = () => {
+        cond = false;
+        console.log(cond);
+        localStorage.setItem("condition", cond);
+    }
+}
+
+clickFaster();
 autoClick();
 increment();
